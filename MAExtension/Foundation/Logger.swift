b20ab394ec15a081a4805f6168a9f8c6c0b23eb6
fengyunjue <ma772528138@qq.com>
2018-05-28 16:00:04 +0800

    V0.1.3 修改Logger

--- MAExtension/Foundation/Logger.swift
+++ MAExtension/Foundation/Logger.swift
@@ -9,46 +9,50 @@
 import Foundation
 import NSLogger
 
-public func LogWarn(_ format: Any?, _ domain: NSLogger.Logger.Domain = Logger.Domain.app,
-             _ filename: String = #file, lineNumber: Int = #line, fnName: String = #function) {
-    LogDefault(format, domain, .warning, filename, lineNumber: lineNumber, fnName: fnName)
-}
-
-public func LogInfo(_ format: Any?, _ domain: NSLogger.Logger.Domain = Logger.Domain.app,
-             _ filename: String = #file, lineNumber: Int = #line, fnName: String = #function) {
-    LogDefault(format, domain, .info, filename, lineNumber: lineNumber, fnName: fnName)
-}
-
-public func LogError(_ format: Any?, _ domain: NSLogger.Logger.Domain = Logger.Domain.app,
-              _ filename: String = #file, lineNumber: Int = #line, fnName: String = #function) {
-    LogDefault(format, domain, .error, filename, lineNumber: lineNumber, fnName: fnName)
-}
-
-private let dateFormatter = DateFormatter()
-private func date() -> String {
-    dateFormatter.dateFormat = "yyyy-MM-dd HH:mm:ss"
-    dateFormatter.locale = Locale(identifier: "en_US_POSIX")
-    return dateFormatter.string(from: Date())
-}
-
-private func LogDefault(_ format: Any?, _ domain: NSLogger.Logger.Domain, _ level: NSLogger.Logger.Level,
-                            _ filename: String = #file, lineNumber: Int = #line, fnName: String = #function) {
-#if DEBUG
-    let str = "\(format ?? "")"
-    
-    Logger.shared.log(domain, level, str, filename, lineNumber, fnName)
-    var levelStr = ""
-    switch level.rawValue {
-    case 0:
-        levelStr = "Error"
-    case 1:
-        levelStr = "Warning"
-    case 3:
-        levelStr = "Info"
-    default:
-        levelStr = "Default"
+struct Log {
+    public static func warn(_ format: Any?, _ domain: NSLogger.Logger.Domain = Logger.Domain.app,
+                        _ filename: String = #file, lineNumber: Int = #line, fnName: String = #function) {
+        Default(format, domain, .warning, filename, lineNumber: lineNumber, fnName: fnName)
     }
     
-    print("[\(date()) \(levelStr) \(URL.init(fileURLWithPath: filename).lastPathComponent):\(lineNumber) \(fnName)]:\(str)")
-#endif
+    public static func info(_ format: Any?, _ domain: NSLogger.Logger.Domain = Logger.Domain.app,
+                        _ filename: String = #file, lineNumber: Int = #line, fnName: String = #function) {
+        Default(format, domain, .info, filename, lineNumber: lineNumber, fnName: fnName)
+    }
+    
+    public static func error(_ format: Any?, _ domain: NSLogger.Logger.Domain = Logger.Domain.app,
+                         _ filename: String = #file, lineNumber: Int = #line, fnName: String = #function) {
+        Default(format, domain, .error, filename, lineNumber: lineNumber, fnName: fnName)
+    }
+    
+    private static let dateFormatter = DateFormatter()
+    private static func date() -> String {
+        dateFormatter.dateFormat = "yyyy-MM-dd HH:mm:ss"
+        dateFormatter.locale = Locale(identifier: "en_US_POSIX")
+        return dateFormatter.string(from: Date())
+    }
+    
+    private static func Default(_ format: Any?, _ domain: NSLogger.Logger.Domain, _ level: NSLogger.Logger.Level,
+                            _ filename: String = #file, lineNumber: Int = #line, fnName: String = #function) {
+        #if DEBUG
+        let str = "\(format ?? "")"
+        
+        Logger.shared.log(domain, level, str, filename, lineNumber, fnName)
+        var levelStr = ""
+        switch level.rawValue {
+        case 0:
+            levelStr = "Error"
+        case 1:
+            levelStr = "Warning"
+        case 3:
+            levelStr = "Info"
+        default:
+            levelStr = "Default"
+        }
+        
+        print("[\(date()) \(levelStr) \(URL.init(fileURLWithPath: filename).lastPathComponent):\(lineNumber) \(fnName)]:\(str)")
+        #endif
+    }
 }
+
+
