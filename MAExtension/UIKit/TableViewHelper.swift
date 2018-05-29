729f91ffd84643c638e2332b3f016ff587c7727a
fengyunjue <ma772528138@qq.com>
2018-05-29 14:55:12 +0800

    V0.1.3 优化TableViewHelper

--- MAExtension/UIKit/TableViewHelper.swift
+++ MAExtension/UIKit/TableViewHelper.swift
@@ -6,23 +6,40 @@
 //
 
 import UIKit
-
-public struct RefreshMode: Equatable {
-    public enum Model {
-        // 插入数据,是否刷新
-        case insert
-        // 删除数据
-        case delete
+public enum ScrollType {
+    case none   // 不滚动
+    case bottom // 滚动到底部
+    case hold   // 滚动到原来的位置,界面上不动
+}
+public enum RefreshMode {
+    case insert(ScrollType) // 插入数据,是否刷新
+    case delete(ScrollType) // 删除数据
+    
+    var type: ScrollType {
+        switch self {
+        case .insert(let type):
+            return type
+        case .delete(let type):
+            return type
+        }
     }
-    public var model: Model
-    public var isScrollBottom: Bool
     
-    public static var insert = RefreshMode.init(model: .insert, isScrollBottom: true)
-    public static var insertNoScroll = RefreshMode.init(model: .insert, isScrollBottom: false)
-    public static var delete = RefreshMode.init(model: .delete, isScrollBottom: true)
+    var isDelete: Bool {
+        switch self {
+        case .delete(_):
+            return true
+        default:
+            return false
+        }
+    }
     
-    public static func == (lhs: RefreshMode, rhs: RefreshMode) -> Bool {
-        return lhs.model == rhs.model
+    var isInsert: Bool {
+        switch self {
+        case .insert(_):
+            return true
+        default:
+            return false
+        }
     }
 }
 
@@ -43,69 +60,82 @@ extension Reloadable where Model: Comparable {
     public mutating func refresh(_ models: [(Model, RefreshMode)]){
         var weakSelf = self
         
-        DispatchQueue.global().async {
-            if models.count == 0 { return }
-            let oldModels = weakSelf.models
-            
-            var addModels: Set<Model> = Set()
-            var deleteModels: Set<Model> = Set()
-            
-            var isScrollBottom: Bool = true
-            models.forEach { (model, type) in
-                isScrollBottom = type.isScrollBottom
-                if type == RefreshMode.insert {
-                    deleteModels.remove(model)
-                    addModels.insert(model)
-                }else{
-                    addModels.remove(model)
-                    deleteModels.insert(model)
-                }
+        if models.count == 0 { return }
+        let oldModels = weakSelf.models
+        
+        var addModels: Set<Model> = Set()
+        var deleteModels: Set<Model> = Set()
+        
+        var scrollType: ScrollType = .none
+        models.forEach { (model, type) in
+            scrollType = type.type
+            if type.isInsert {
+                deleteModels.remove(model)
+                addModels.insert(model)
+            }else{
+                addModels.remove(model)
+                deleteModels.insert(model)
             }
-            // 处理数据
-            weakSelf.models.remove(contentsOf: deleteModels)
-            let orders = weakSelf.models.insertOrder(contentsOf: addModels)
+        }
+        
+        // 处理数据
+        weakSelf.models.remove(contentsOf: deleteModels)
+        
+        let orders = weakSelf.models.insertOrder(contentsOf: addModels)
+        
+        let deleteIndexPaths: [IndexPath] = deleteModels.compactMap{ (model) -> IndexPath? in
+            if let index = oldModels.index(of: model) {
+                return IndexPath.init(row: index, section: 0)
+            }else{
+                return nil
+            }
+        }
+        let reloadIndexPaths: [IndexPath] = orders.1.compactMap { (model) -> IndexPath? in
+            if let index = oldModels.index(of: model) {
+                return IndexPath.init(row: index, section: 0)
+            }else{
+                return nil
+            }
+        }
+        let insertIndexPaths: [IndexPath] = orders.0.compactMap { (model) -> IndexPath? in
+            if let index = weakSelf.models.index(of: model) {
+                return IndexPath.init(row: index, section: 0)
+            }else{
+                return nil
+            }
+        }
+        
+        var bottomIndexPath: IndexPath? = nil
+        // bottom的偏移量
+        var offsetInset: CGFloat? = nil
+        if let cell = self.reloadTableView.visibleCells.last, let indexPath = self.reloadTableView.indexPath(for: cell), indexPath.row < oldModels.count {
+            let model = oldModels[indexPath.row]
+            if let index = self.models.index(of: model) {
+                bottomIndexPath = IndexPath.init(row: index, section: 0)
+                offsetInset = cell.frame.maxY - self.reloadTableView.frame.height - self.reloadTableView.contentOffset.y
+            }
+        }
+        UIView.noAnimation {
+            weakSelf.reloadTableView.beginUpdates()
+            if deleteIndexPaths.count > 0{
+                weakSelf.reloadTableView.deleteRows(at: deleteIndexPaths, with: .none)
+            }
+            if insertIndexPaths.count > 0{
+                weakSelf.reloadTableView.insertRows(at: insertIndexPaths, with: .none)
+            }
+            if reloadIndexPaths.count > 0{
+                weakSelf.reloadTableView.reloadRows(at: reloadIndexPaths, with: .none)
+            }
+            weakSelf.reloadTableView.endUpdates()
             
-            let deleteIndexPaths: [IndexPath] = deleteModels.compactMap{ (model) -> IndexPath? in
-                if let index = oldModels.index(of: model) {
-                    return IndexPath.init(row: index, section: 0)
-                }else{
-                    return nil
-                }
+            if scrollType == .bottom {
+                weakSelf.reloadTableView.scrollToRow(at: IndexPath.init(row: weakSelf.models.count-1, section: 0), at: .bottom, animated: true)
+            }else if scrollType == .hold && bottomIndexPath != nil && offsetInset != nil{
+                weakSelf.reloadTableView.scrollToRow(at: bottomIndexPath!, at: .none, animated: false)
+                var offset = self.reloadTableView.contentOffset
+                offset.y -= offsetInset!
+                self.reloadTableView.contentOffset = offset
             }
-            let reloadIndexPaths: [IndexPath] = orders.1.compactMap { (model) -> IndexPath? in
-                if let index = oldModels.index(of: model) {
-                    return IndexPath.init(row: index, section: 0)
-                }else{
-                    return nil
-                }
-            }
-            let insertIndexPaths: [IndexPath] = orders.0.compactMap { (model) -> IndexPath? in
-                if let index = weakSelf.models.index(of: model) {
-                    return IndexPath.init(row: index, section: 0)
-                }else{
-                    return nil
-                }
-            }
-            
-            DispatchQueue.main.async {
-                UIView.noAnimation {
-                    weakSelf.reloadTableView.beginUpdates()
-                    if deleteIndexPaths.count > 0{
-                        weakSelf.reloadTableView.deleteRows(at: deleteIndexPaths, with: .none)
-                    }
-                    if insertIndexPaths.count > 0{
-                        weakSelf.reloadTableView.insertRows(at: insertIndexPaths, with: .none)
-                    }
-                    if reloadIndexPaths.count > 0{
-                        weakSelf.reloadTableView.reloadRows(at: reloadIndexPaths, with: .none)
-                    }
-                    weakSelf.reloadTableView.endUpdates()
-                    if isScrollBottom {
-                        weakSelf.reloadTableView.scrollToRow(at: IndexPath.init(row: weakSelf.models.count-1, section: 0), at: .bottom, animated: true)
-                    }
-                }
-            }
-            
         }
     }
     public func scrollBottom(_ animated: Bool = true) {
