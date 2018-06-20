83cf5a79cf283050670b58178b08ac24a426ebc4
fengyunjue <ma772528138@qq.com>
2018-06-20 17:09:00 +0800

    V0.1.14 优化ArrayHelper.swift的insertOrder提高效率

--- MAExtension/Foundation/ArrayHelper.swift
+++ MAExtension/Foundation/ArrayHelper.swift
@@ -8,6 +8,20 @@
 import Foundation
 
 // MARK: - Array 拓展
+extension Array {
+    // 随机打乱数据
+    func shuffle() -> Array {
+        var list = self
+        for index in 0..<list.count {
+            let newIndex = Int(arc4random_uniform(UInt32(list.count-index))) + index
+            if index != newIndex {
+                list.swapAt(index, newIndex)
+            }
+        }
+        return list
+    }
+}
+
 extension Array where Element: Equatable {
     
     @discardableResult
@@ -20,40 +34,63 @@ extension Array where Element: Equatable {
             return nil
         }
     }
-    public mutating func remove<S>(contentsOf oldElements: S) where Element == S.Element, S : Sequence {
+    
+    @discardableResult
+    public mutating func remove<S>(contentsOf oldElements: S) -> [Int] where Element == S.Element, S : Sequence {
+        var indexs: [Int] = []
         oldElements.forEach { (element) in
-            self.remove(element: element)
+            if let index = self.remove(element: element) {
+                indexs.append(index)
+            }
         }
+        return indexs
     }
 }
 
 extension Array where Element: Comparable {
     /// 返回: (位置, 插入是true更新是false)
     @discardableResult
-    public mutating func insertOrder(_ element: Element) -> (Int, Bool){
-        if self.contains(element) {
-            let index = self.index(of: element)!
+    public mutating func insertOrder(_ element: Element, _ asc: Bool = true) -> (Int, Bool){
+        
+        var index: Int = 0
+        var hasValue = false
+        
+        var left = 0
+        var right = self.count - 1
+        
+        while left <= right {
+            let mid = (left + right) / 2
+            let value = self[mid]
+            if value == element {
+                index = mid
+                hasValue = true
+                break
+            }else if value < element {
+                left = asc ? mid + 1 : left
+                index = asc ? mid + 1 : mid
+                right = asc ? right : mid - 1
+            }else if value > element {
+                left = !asc ? mid + 1 : left
+                index = !asc ? mid + 1 : mid
+                right = !asc ? right : mid - 1
+            }
+        }
+        
+        if hasValue {
             self[index] = element
             return (index, false)
         }else{
-            var index: Int = 0
-            for (i, value) in self.enumerated().reversed() {
-                if element > value {
-                    index = i+1
-                    break
-                }
-            }
             self.insert(element, at: index)
             return (index, true)
         }
     }
     
-    public mutating func insertOrder<S>(contentsOf newElements: S) -> ([Element], [Element]) where Element == S.Element, S : Sequence{
+    public mutating func insertOrder<S>(contentsOf newElements: S, _ asc: Bool = true) -> ([Element], [Element]) where Element == S.Element, S : Sequence{
         var inserts: [Element] = []
         var reloads: [Element] = []
         
         newElements.forEach { (m) in
-            let result = self.insertOrder(m)
+            let result = self.insertOrder(m, asc)
             if result.1 {
                 inserts.append(m)
             }else {
@@ -62,5 +99,4 @@ extension Array where Element: Comparable {
         }
         return (inserts, reloads)
     }
-    
 }
--- MAExtension/UIKit/TableViewHelper.swift
+++ MAExtension/UIKit/TableViewHelper.swift
@@ -1,58 +1,30 @@
-//
-//  TableViewHelper.swift
-//  MAExtension
+
 //
 //  Created by admin on 2018/5/28.
 //
 
 import UIKit
+
+extension IndexPath {
+    init?(row: Int?, section: Int?) {
+        if let row = row, let section = section {
+            self.init(row: row, section: section)
+        }else{
+            return nil
+        }
+    }
+}
+
 public enum ScrollType {
     case none   // 不滚动
     case bottom // 滚动到底部
     case hold   // 滚动到原来的位置,界面上不动
 }
-public enum RefreshMode {
-    case insert(ScrollType) // 插入数据
-    case delete(ScrollType) // 删除数据
-    
-    public var type: ScrollType {
-        get {
-            switch self {
-            case .insert(let type):
-                return type
-            case .delete(let type):
-                return type
-            }
-        }
-        set (newType) {
-            switch self {
-            case .insert(_):
-                self = .insert(newType)
-            case .delete(_):
-                self = .delete(newType)
-            }
-        }
-    }
-    
-    public var isDelete: Bool {
-        switch self {
-        case .delete(_):
-            return true
-        default:
-            return false
-        }
-    }
-    
-    public var isInsert: Bool {
-        switch self {
-        case .insert(_):
-            return true
-        default:
-            return false
-        }
-    }
-}
 
+public enum RefreshMode {
+    case insert // 插入数据
+    case delete // 删除数据
+}
 
 // MARK: - UITableView扩展
 public protocol Reloadable {
@@ -62,62 +34,40 @@ public protocol Reloadable {
     var models: [Model] { get set }
     var reloadTableView: UITableView { get }
     
-    mutating func refresh(_ models: [(Model, RefreshMode)])
+    mutating func refresh(_ models: [(Model, RefreshMode)], scrollType: ScrollType)
 }
 
 extension Reloadable where Model: Comparable {
     
-    public mutating func refresh(_ models: [(Model, RefreshMode)]){
+    public mutating func refresh(_ models: [(Model, RefreshMode)], scrollType: ScrollType = .bottom) {
         
         if models.count == 0 { return }
+        
         let oldModels = self.models
         
         var addModels: Set<Model> = Set()
         var deleteModels: Set<Model> = Set()
         
-        var scrollType: ScrollType = .none
         models.forEach { (model, type) in
-            scrollType = type.type
-            if type.isInsert {
+            switch type {
+            case .insert:
                 deleteModels.remove(model)
                 addModels.insert(model)
-            }else{
+            case .delete:
                 addModels.remove(model)
                 deleteModels.insert(model)
             }
         }
         
-        // 处理数据
-        self.models.remove(contentsOf: deleteModels)
+        // 需要删除的数据
+        let deleteIndexPaths: [IndexPath] =  self.models.remove(contentsOf: deleteModels).map{IndexPath.init(row: $0, section: 0)}
         
+        // 需要添加或更新的数据
         let orders = self.models.insertOrder(contentsOf: addModels)
         
-        let deleteIndexPaths: [IndexPath] = deleteModels.compactMap{ (model) -> IndexPath? in
-            if let index = oldModels.index(of: model) {
-                return IndexPath.init(row: index, section: 0)
-            }else{ return nil }
-        }
-        let reloadIndexPaths: [IndexPath] = orders.1.compactMap { (model) -> IndexPath? in
-            if let index = oldModels.index(of: model) {
-                return IndexPath.init(row: index, section: 0)
-            }else{ return nil }
-        }
-        let insertIndexPaths: [IndexPath] = orders.0.compactMap { (model) -> IndexPath? in
-            if let index = self.models.index(of: model) {
-                return IndexPath.init(row: index, section: 0)
-            }else{ return nil }
-        }
+        let reloadIndexPaths: [IndexPath] = orders.1.compactMap{IndexPath.init(row: oldModels.index(of: $0), section: 0)}
+        let insertIndexPaths: [IndexPath] = orders.0.compactMap{IndexPath.init(row: self.models.index(of: $0), section: 0)}
         
-        var bottomIndexPath: IndexPath? = nil
-        // bottom的偏移量
-        var offsetInset: CGFloat? = nil
-        if let cell = self.reloadTableView.visibleCells.last, let indexPath = self.reloadTableView.indexPath(for: cell), indexPath.row < oldModels.count {
-            let model = oldModels[indexPath.row]
-            if let index = self.models.index(of: model) {
-                bottomIndexPath = IndexPath.init(row: index, section: 0)
-                offsetInset = cell.frame.maxY - self.reloadTableView.frame.height - self.reloadTableView.contentOffset.y
-            }
-        }
         UIView.noAnimation {
             self.reloadTableView.beginUpdates()
             if deleteIndexPaths.count > 0{
@@ -133,14 +83,24 @@ extension Reloadable where Model: Comparable {
             
             if scrollType == .bottom {
                 self.reloadTableView.scrollToRow(at: IndexPath.init(row: self.models.count-1, section: 0), at: .bottom, animated: true)
-            }else if scrollType == .hold && bottomIndexPath != nil && offsetInset != nil{
-                self.reloadTableView.scrollToRow(at: bottomIndexPath!, at: .none, animated: false)
-                var offset = self.reloadTableView.contentOffset
-                offset.y -= offsetInset!
-                self.reloadTableView.contentOffset = offset
+            }else if scrollType == .hold {
+                // 计算出tableView界面上显示的最后一个cell,在更新后的位置,并重新滚动到该位置
+                if let cell = self.reloadTableView.visibleCells.last, let indexPath = self.reloadTableView.indexPath(for: cell), indexPath.row < oldModels.count, let index = self.models.index(of: oldModels[indexPath.row]) {
+                    // 最后一个cell的IndexPath
+                    let bottomIndexPath = IndexPath.init(row: index, section: 0)
+                    // bottom的偏移量
+                    let offsetInset = cell.frame.maxY - self.reloadTableView.frame.height - self.reloadTableView.contentOffset.y
+                    
+                    // 滚动
+                    self.reloadTableView.scrollToRow(at: bottomIndexPath, at: .none, animated: false)
+                    var offset = self.reloadTableView.contentOffset
+                    offset.y -= offsetInset
+                    self.reloadTableView.contentOffset = offset
+                }
             }
         }
     }
+    
     public func scrollBottom(_ animated: Bool = true) {
         self.reloadTableView.scrollToRow(at: IndexPath.init(row: self.models.count-1, section: 0), at: .bottom, animated: animated)
     }
